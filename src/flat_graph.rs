3d146779a459bd787d4c825deb2517259a52e4eb
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2022-01-14 11:23:41 +0100

    Secure random for graph generation

--- src/flat_graph.rs
+++ src/flat_graph.rs
@@ -4,6 +4,8 @@ use rand::Rng;
 use std::collections::VecDeque;
 use std::fmt;
 use std::fmt::Formatter;
+use rand_chacha::ChaChaRng;
+use rand_chacha::rand_core::SeedableRng;
 
 #[derive(Clone)]
 pub(crate) struct FlatGraph {
@@ -28,9 +30,10 @@ impl FlatGraph {
         red_node_probability: f32,
         red_edge_probability: f32,
         left_directed_edge_probability: f32,
-    ) { // TODO: secure random
+    ) {
+        let mut chacha_secure_rng = ChaChaRng::from_entropy();
         for node in self.nodes.iter_mut() {
-            match rand::thread_rng().gen_range(0f32..1f32) {
+            match chacha_secure_rng.gen_range(0f32..1f32) {
                 x if x < red_node_probability => {
                     *node = Some(Color::RED);
                 }
@@ -40,11 +43,11 @@ impl FlatGraph {
             }
         }
         for vertex in self.vertices.iter_mut() {
-            let vertex_color = match rand::thread_rng().gen_range(0f32..1f32) {
+            let vertex_color = match chacha_secure_rng.gen_range(0f32..1f32) {
                 x if x < red_edge_probability => Color::RED,
                 _ => Color::BLUE,
             };
-            let vertex_direction = match rand::thread_rng().gen_range(0f32..1f32) {
+            let vertex_direction = match chacha_secure_rng.gen_range(0f32..1f32) {
                 x if x < left_directed_edge_probability => VertexDirection::LEFT,
                 _ => VertexDirection::RIGHT,
             };
