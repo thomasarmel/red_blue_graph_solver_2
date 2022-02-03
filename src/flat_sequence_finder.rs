4ded0d01656296c7c937179b6c8338d254d8eb00
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2022-02-03 17:38:58 +0100

    make flat_sequence_finder immutable

--- src/flat_sequence_finder.rs
+++ src/flat_sequence_finder.rs
@@ -1,36 +1,36 @@
 #![allow(dead_code)]
 
+use std::cell::RefCell;
 use crate::flat_graph::{Color, FlatGraph, VertexDirection};
 use std::collections::VecDeque;
 
 pub struct FlatSequenceFinder {
-    flat_graph_copy: FlatGraph,
-    pub sequence_max: VecDeque<usize>,
+    flat_graph_copy: RefCell<FlatGraph>,
     color_to_remove: Color,
 }
 
 impl FlatSequenceFinder {
     pub fn new(flat_graph: &FlatGraph, color_to_remove: Color) -> Self {
         FlatSequenceFinder {
-            flat_graph_copy: flat_graph.clone(),
-            sequence_max: VecDeque::new(),
+            flat_graph_copy: RefCell::new(flat_graph.clone()),
             color_to_remove,
         }
     }
 
-    fn sequence_max_push_and_remove_util(&mut self, current_node: usize) {
-        self.sequence_max.push_back(current_node);
-        self.flat_graph_copy.remove_node(current_node).unwrap(); // May be useless with a stack
+    fn sequence_max_push_and_remove_util(&self, current_node: usize, sequence_max: &mut VecDeque<usize>) {
+        sequence_max.push_back(current_node);
+        self.flat_graph_copy.borrow_mut().remove_node(current_node).unwrap(); // May be useless with a stack
     }
 
     fn find_nodes_to_remove_before_current(
-        &mut self,
+        &self,
         current_node: usize,
         finding_direction: VertexDirection,
+        sequence_max: &mut VecDeque<usize>
     ) {
         // Could MACROS be used here ?
         let is_vertex_left_or_right = |vertex_direction: VertexDirection, edge_id: usize| -> bool {
-            self.flat_graph_copy
+            self.flat_graph_copy.borrow()
                 .get_vertex(edge_id)
                 .unwrap()
                 .unwrap()
@@ -54,9 +54,9 @@ impl FlatSequenceFinder {
             VertexDirection::RIGHT => current_node,
         };
         loop {
-            if self.flat_graph_copy.node_exists(tmp_node)
-                && self.flat_graph_copy.get_node(tmp_node).unwrap().unwrap() == self.color_to_remove
-                && self.flat_graph_copy.vertex_exists(tmp_edge)
+            if self.flat_graph_copy.borrow().node_exists(tmp_node)
+                && self.flat_graph_copy.borrow().get_node(tmp_node).unwrap().unwrap() == self.color_to_remove
+                && self.flat_graph_copy.borrow().vertex_exists(tmp_edge)
                 && is_vertex_left_or_right(finding_direction, tmp_edge)
             {
                 match finding_direction {
@@ -86,7 +86,7 @@ impl FlatSequenceFinder {
                 }
                 shift_node_and_vertex(&mut tmp_node, &mut tmp_edge);
                 while BACK_EDGE_COMPARE_CURRENT(finding_direction, current_node, tmp_node) {
-                    self.sequence_max_push_and_remove_util(tmp_node);
+                    self.sequence_max_push_and_remove_util(tmp_node, sequence_max);
                     if finding_direction == VertexDirection::RIGHT
                         && (tmp_node == 0 || tmp_edge == 0)
                     {
@@ -100,11 +100,11 @@ impl FlatSequenceFinder {
     }
 
     fn node_and_vertex_have_color(&self, node: usize, vertex: usize) -> bool {
-        self.flat_graph_copy.node_exists(node)
-            && self.flat_graph_copy.get_node(node).unwrap().unwrap() == self.color_to_remove
-            && self.flat_graph_copy.vertex_exists(vertex)
+        self.flat_graph_copy.borrow().node_exists(node)
+            && self.flat_graph_copy.borrow().get_node(node).unwrap().unwrap() == self.color_to_remove
+            && self.flat_graph_copy.borrow().vertex_exists(vertex)
             && self
-                .flat_graph_copy
+                .flat_graph_copy.borrow()
                 .get_vertex(vertex)
                 .unwrap()
                 .unwrap()
@@ -113,7 +113,7 @@ impl FlatSequenceFinder {
     }
 
     fn node_may_be_interesting_to_remove(
-        &mut self,
+        &self,
         node: usize,
         finding_direction: VertexDirection,
     ) -> bool {
@@ -124,12 +124,12 @@ impl FlatSequenceFinder {
             VertexDirection::LEFT => node - 1,
             VertexDirection::RIGHT => node,
         };
-        if !self.flat_graph_copy.vertex_exists(edge_tmp) {
+        if !self.flat_graph_copy.borrow().vertex_exists(edge_tmp) {
             return false;
         }
         if finding_direction
             != self
-                .flat_graph_copy
+                .flat_graph_copy.borrow()
                 .get_vertex(edge_tmp)
                 .unwrap()
                 .unwrap()
@@ -142,30 +142,31 @@ impl FlatSequenceFinder {
             VertexDirection::RIGHT => node + 1,
         };
         self.node_and_vertex_have_color(node, edge_tmp)
-            && self.flat_graph_copy.node_exists(node_dst)
-            && self.flat_graph_copy.get_node(node_dst).unwrap().unwrap() != self.color_to_remove
+            && self.flat_graph_copy.borrow().node_exists(node_dst)
+            && self.flat_graph_copy.borrow().get_node(node_dst).unwrap().unwrap() != self.color_to_remove
     }
 
-    pub fn calculate_sequence_max(&mut self) {
+    pub fn calculate_sequence_max(&self) -> VecDeque<usize> {
+        let mut sequence_max = VecDeque::new();
         let mut current_node: usize = 0;
-        while current_node < self.flat_graph_copy.max_capacity {
+        while current_node < self.flat_graph_copy.borrow().max_capacity {
             if self.node_may_be_interesting_to_remove(current_node, VertexDirection::RIGHT) {
                 if self.node_may_be_interesting_to_remove(current_node, VertexDirection::LEFT) {
-                    self.sequence_max_push_and_remove_util(current_node);
+                    self.sequence_max_push_and_remove_util(current_node, &mut sequence_max);
                     current_node -= 1;
                 } else {
-                    self.sequence_max_push_and_remove_util(current_node);
+                    self.sequence_max_push_and_remove_util(current_node, &mut sequence_max);
                     current_node += 1;
                 }
             } else {
                 if self.node_may_be_interesting_to_remove(current_node, VertexDirection::LEFT) {
-                    self.find_nodes_to_remove_before_current(current_node, VertexDirection::RIGHT);
-                    self.sequence_max_push_and_remove_util(current_node);
+                    self.find_nodes_to_remove_before_current(current_node, VertexDirection::RIGHT, &mut sequence_max);
+                    self.sequence_max_push_and_remove_util(current_node, &mut sequence_max);
                     current_node -= 1;
                 } else {
-                    if self.flat_graph_copy.node_exists(current_node)
+                    if self.flat_graph_copy.borrow().node_exists(current_node)
                         && self
-                            .flat_graph_copy
+                            .flat_graph_copy.borrow()
                             .get_node(current_node)
                             .unwrap()
                             .unwrap()
@@ -174,12 +175,14 @@ impl FlatSequenceFinder {
                         self.find_nodes_to_remove_before_current(
                             current_node,
                             VertexDirection::RIGHT,
+                            &mut sequence_max
                         );
-                        self.sequence_max_push_and_remove_util(current_node);
+                        self.sequence_max_push_and_remove_util(current_node, &mut sequence_max);
                     }
                     current_node += 1;
                 }
             }
         }
+        sequence_max
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,19 @@ use std::time::Instant;
 use red_blue_graph::{flat_graph, flat_sequence_finder};
 
 fn main() {
-    let mut flat_graph = flat_graph::FlatGraph::new(5000000);
+    let mut flat_graph = flat_graph::FlatGraph::new(10);
     flat_graph.generate_random(0.5, 0.5, 0.5);
     println!("Test on a graph of size {}", flat_graph.max_capacity);
 
-    //println!("{}", flat_graph);
-    let mut flat_sequence_finder =
+    println!("{}", flat_graph);
+    let flat_sequence_finder =
         flat_sequence_finder::FlatSequenceFinder::new(&flat_graph, flat_graph::Color::RED);
     let start = Instant::now();
-    flat_sequence_finder.calculate_sequence_max();
+    let sequence_max = flat_sequence_finder.calculate_sequence_max();
     let duration = start.elapsed();
-    /*for node in flat_sequence_finder.sequence_max.iter() {
+    for node in sequence_max.iter() {
         println!("{}", node);
-    }*/
+    }
+    println!("{}", flat_graph);
     println!("{} nanoseconds elapsed", duration.as_nanos());
 }
